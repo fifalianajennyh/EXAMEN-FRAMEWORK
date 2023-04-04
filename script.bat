28b84ff067203b96177f36a01508a89899862a24
fifalianajennyh <124344145+fifalianajennyh@users.noreply.github.com>
2023-04-04 04:39:31 -0500

    ok4testjar

---
+++ script.bat
@@ -0,0 +1,34 @@
+@echo off
+
+rem compiling source code
+javac -classpath .\lib\servlet-api.jar;.\lib\r-w-x_file.jar -d .\bin\framework --enable-preview --release 19 %cd%\framework\src\*.java
+cd .\bin\framework
+
+rem exporting the framework to a jar file
+jar cvf ..\..\fw.jar *
+cd ..\..\
+
+rem creating the directory structure for the project test to deploy
+mkdir .\temp .\temp\WEB-INF .\temp\WEB-INF\classes .\temp\WEB-INF\lib
+
+rem copying jar file to the project library and the web.xml file
+copy .\fw.jar .\temp\WEB-INF\lib\ 
+copy .\lib\*.jar .\temp\WEB-INF\lib\
+copy .\TestFramework\web.xml .\temp\WEB-INF\
+
+rem compiling models and other user necessity to the project classes directory
+javac -classpath .\fw.jar -d .\temp\WEB-INF\classes --enable-preview --release 19 %cd%\TestFramework\*.java
+cd .\temp
+
+rem exporting the temp directory to a war file
+jar cvf ..\testFramework.war *
+cd ..
+
+rem deploying the war file to Tomcat
+copy .\testFramework.war "C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps\"
+
+rem removing temp directory
+rmdir /s /q .\temp
+
+echo Deployment of testFramework.war completed.
+pause
